1e53347b9c3c6f5020584ab00ab63b0bfcc25b76
Brian Kanduli <briankanduli@gmail.com>
2019-05-10 10:07:09 +0400

    Initial commit

---
+++ jenkins/src/main/java/com/kanduli/jenkins/jenkins/JenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.kanduli.jenkins.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsApplication.class, args);
+    }
+
+}
---
+++ jenkins/src/test/java/com/kanduli/jenkins/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kanduli.jenkins.jenkins;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
